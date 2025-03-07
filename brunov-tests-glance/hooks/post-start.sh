d03e6953faf08c7d5b900a28179bc5ddfeffb118
Bruno Vieira <76965972+BrunoVieira003@users.noreply.github.com>
2025-03-07 01:58:04 -0300

    try pre-configured folder

--- brunov-tests-glance/hooks/post-start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Creating glance.yml file on host..."
-
-# Create the file on the host system (ensure the path is accessible and writable by the container)
-touch "${APP_DATA_DIR}/config/glance.yml"
-
-echo "File created on the host system."
