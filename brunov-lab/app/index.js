97787aed2df0096d642827ce30813c2d5bde917d
BrunoVieira003 <brunosilverio01@gmail.com>
2025-06-14 19:29:13 -0300

    testing widgets

---
+++ brunov-lab/app/index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/stats', (req, res) => {
+  res.json({
+    items: [
+      { title: "Alcelora", text: "Bom pra tosse", subtext: "fruta" },
+      { title: "Alcelora", text: "Bom pra gripe", subtext: "fruta" },
+      { title: "Limao", text: "Natural", subtext: "fruta" },
+      { title: "Limao", text: "nao natural", subtext: "fruta" }
+    ],
+    link: "mylink",
+    refresh: 5000,
+    type: "four-stats"
+  })
+})
+
+app.listen(port, () => {
+  console.log(`Mock API listening at http://localhost:${port}`)
+})
